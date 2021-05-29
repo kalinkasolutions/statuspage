6ea22a7bbded850ea48bb75d1d764e295aae4a69
kalinka <kalinka@kalinka.ch>
2021-05-29 11:54:49 +0200

    remove url from env

--- status_page_backend/src/config.ts
+++ status_page_backend/src/config.ts
@@ -5,14 +5,12 @@ dotenv.config();
 interface EnvConfig {
   uptimeRobotReadonlyKey: string;
   pageName: string;
-  url: string;
 }
 
 const config = (): EnvConfig => {
   return {
     uptimeRobotReadonlyKey: process.env.UPTIME_ROBOT_READONLY_KEY,
     pageName: process.env.PAGE_NAME,
-    url: process.env.URL,
   };
 };
 
--- status_page_backend/src/server.ts
+++ status_page_backend/src/server.ts
@@ -2,25 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import api from "./api";
 import cors from "cors";
-import fs from "fs";
 import path from "path";
-import config from "./config";
-
-const files = fs.readdirSync(path.join(__dirname, "../build/static/js"));
-files.forEach((file) => {
-  if (file.endsWith(".js")) {
-    const filecontent = fs.readFileSync(
-      path.join(__dirname, "../build/static/js", file),
-      "utf-8"
-    );
-    if (filecontent.indexOf("http://localhost:3000") > -1) {
-      fs.writeFileSync(
-        path.join(__dirname, "../build/static/js", file),
-        filecontent.split("http://localhost:3000").join(config().url)
-      );
-    }
-  }
-});
 
 const app = express();
 app.use(helmet());
